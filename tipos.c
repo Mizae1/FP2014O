48efa2a262144a1a27d24c9f0d8660cdba188252
japier <japier.07@gmail.com>
2014-09-22 08:01:10 -0500

    Tipos de datos
    Datos y su forma de representarlos en entradas y salidas

--- tipos.c
+++ tipos.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	//int n = 65;
-	//char n = 'A';
-	float n = 6.36;
-	printf("%d\n", n);
+	int n = 0;
+	char n1 = 'b';
+	float n3 = 0;
+	double n5 = 0;
+	short n7 = 0;
+	scanf("%i", &n); //Int
+	scanf("%c", &n1); //Char
+	scanf("%f", &n3); //Float
+	scanf("%f", &n5); //Double, tambien es valido con %d
+	scanf("%i", &n7); //Short
+	printf("%d\n", n); 
 	return 0;
 }
