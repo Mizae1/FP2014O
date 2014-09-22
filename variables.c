95ec7ec854feaf00d07921323d02b5ac82876018
japier <japier.07@gmail.com>
2014-09-22 08:00:20 -0500

    Variables validas
    Muestra las variables validas y las invalidas

---
+++ variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	/*Valias*/
+	int _valida;
+	char variabe;
+	float var1abl3;
+	int _int;
+	int hola_mundo;
+	int adiosMundo;
+
+	/*No validas*/
+	int 1moxxo;
+	int acción;
+	double ñ;
+	char if;
+
+
+	return 0;
+}
