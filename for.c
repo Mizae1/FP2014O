50cc2723507a773f4c87314660aa7a03975a8e53
japier <japier.07@gmail.com>
2014-10-06 08:05:35 -0500

    Agregar comentarios al for

--- for.c
+++ for.c
@@ -2,15 +2,17 @@
 int main(){
 	int x = 0;
 	scanf("%d", &x);
-	while(x < 10){
+	while(x < 10){ // Se ejecuta mientras la condicion se siga cumpliendo
 		printf("%d\n", x);
 		x = x + 2;
 	}
 
-	do {
+	do { // Se ejecuta minimo una vez, sin importar que la condicion no se cumpla
 		printf("Numero\n");
-	}while(x == 4);
-	for (x = 1; x<10 ; x += 2){
+	}while(x == 4); //Tiene que tener ; al final del while
+
+
+	for (x = 1; x<10 ; x += 2){ //for(inicio, conficion de salida, incremento)
 		printf("%d\n", x);
 	}
 	return 0;
