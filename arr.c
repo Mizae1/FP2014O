6e46145d5b3b8082a7f6e0f44f59036b47a51f03
japier <japier.07@gmail.com>
2014-10-20 08:21:08 -0500

    Arreglos 1
    Las direcciones de los arreglos

---
+++ arr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int jesus = 7;
+	int mr_satan[3] = {6, 6, 6};
+    int arr[10];
+    scanf("%d", 0x0028FEEC);
+    printf("%p\n", &arr[0]);
+    printf("%p\n", (arr+2));
+    printf("%p\n", &arr[2]);
+    printf("%d", arr[2]);
+	return 0;
+}
