59789559fe005c4f4ad9f79559f2691dc20fe036
japier <japier.07@gmail.com>
2014-10-22 08:09:11 -0500

    Arreglo de Caracteres 2
    Checar el numero de vocales

--- arr_caracteres.c
+++ arr_caracteres.c
@@ -3,15 +3,20 @@
 int main()
 {
 	char arr[100] = "Hola, Bienvenidos";
-	int i = 0, j;
+	int i = 0, j, n_vocales = 0;
+	char v[5] = {'a','e','i','o','u'};
 	printf("--%s--\n", arr);
 	gets(arr);
 	while (arr[i] != '\0'){
+		
+		for (j = 0; j < 5; ++j)
+		{
+			if(arr[i] == v[j]){
+				n_vocales++;
+			}
+		}
 		i++;
 	}
-	for (j = i-1; j >= 0; --j)
-	{
-		printf("%c", arr[j]);
-	}
+	printf("%d\n", n_vocales);
 	return 0;
 }
